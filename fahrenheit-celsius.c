563e54657e29ae422388918f7bf04df5ee3ddb63
Raddatz <rayandematos@gmail.com>
2023-11-02 21:35:21 -0300

    Changes in the output formating and some data types

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -2,26 +2,28 @@
 
 /* Print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., n
-    u can change the stop two, so:
+    u can change the step two, so:
     for fahr = 0, 0 + step, ..., n*/
 int main(){
 
-    int fahr, celsius; 
+    int fahr = 0;
+    float celsius; 
     int upper, step;
-
-    fahr = 0;
     
-    printf("\nStep: ");
+    printf("\n-  Step: ");
     scanf("%d", &step);
 
-    printf("Max: ");
+    printf("-  Max: ");
     scanf("%d", &upper);
 
+    printf("\n\t F   |    C");
+    printf("\n       ---------------");
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("\n%d\t%d", fahr, celsius);
+        celsius = (float) 5 * (fahr - 32) / 9;
+        printf("\n\t%3d  |  %5.1f", fahr, celsius);
         fahr += step;
     }
 
+    printf("\n");
     return 0;
 }
